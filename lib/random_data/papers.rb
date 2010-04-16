08c774c862f5961e2e123c6b2b132af8823d833e
Bruno Malvestuto <bruno.malvestuto@gmail.com>
2010-04-16 17:00:10 -0300

    Some bug fixes

--- lib/random_data/papers.rb
+++ lib/random_data/papers.rb
@@ -1,18 +1,19 @@
 module RandomData
 
-  # Methods to create Brazilian CPF number
+  # Method to create Brazilian CPF number
   module Papers
     def cpf
-      numbers = Array.new(9) {rand(10)} 
+      numbers = Array.new(9) {rand(10)}
       2.times do
-        start = 2 
+        start = 2
         digit = 0
-        for i in start..numbers.size  do  
-          digit +=  numbers.reverse[i-start] * i 
-        end 
-        digit = 11 - (digit % 11) 
+        for i in start..(numbers.size+1) do
+          digit +=  (numbers.reverse[i-start] * i)
+        end
+
+        digit = 11 - (digit % 11)
         digit = 0 if digit >= 10
-        
+
         start += 1
         numbers << digit
       end
@@ -20,7 +21,7 @@ module RandomData
     end
 
     def formated_cpf
-      cpf = self.cpf
+      cpf = cpf()
       "#{cpf[0..2]}.#{cpf[3..5]}.#{cpf[6..8]}-#{cpf[9..10]}"
     end
   end
